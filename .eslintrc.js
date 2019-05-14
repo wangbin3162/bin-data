67b6fbe8ff5ded852bd1a8302d79069b2bc2408a
wangbin3162 <33084011+wangbin3162@users.noreply.github.com>
2019-05-14 10:28:01 +0800

    first commit
    初始化基本项目配置依赖，空壳项目

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  'extends': [
+    'plugin:vue/essential',
+    '@vue/standard'
+  ],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'indent': 'off',
+    'space-before-function-paren': 'off',
+    'no-unused-vars': 'off'
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  }
+}
